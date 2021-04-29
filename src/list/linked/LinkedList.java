8bf5622c109ea2f8d89ba1a3747875bb558b5ea2
jungsunhwan <znzn9292@gmail.com>
2021-04-29 23:51:23 +0900

    add: LinkedList

---
+++ src/list/linked/LinkedList.java
@@ -0,0 +1,29 @@
+package list.linked;
+
+public class LinkedList {
+    private Node head;
+    private Node tail;
+    private int size = 0;
+
+    private class Node {
+        private Object data;
+        private Node next;
+        public Node(Object input) {
+            this.data = input;
+            this.next = null;
+        }
+        public String toString() {
+            return String.valueOf(this.data);
+        }
+    }
+
+    public void addFirst(Object input) {
+        Node newNode = new Node(input);
+        newNode.next = head;
+        head = newNode;
+        size ++;
+        if(head.next == null) {
+            tail = head;
+        }
+    }
+}
